65e9eca0b4fddf0b55bde9c84873f0aa337ab10d
arroyopablo <pablo.arroyo@correounivalle.edu.co>
2019-07-03 23:28:52 -0500

    subiendo solucion del sexto  punto

---
+++ Ejercicios-Programacion-C05/pregunta6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main()
+{
+    int rc = fork();
+    
+    if (rc == 0) // hijo
+    {
+        printf("Soy el proceso hijo. Mi PID es %d\n", getpid());
+        exit(0);
+    }
+    else if (rc > 0) // padre
+    {
+        int estado;
+        waitpid(rc, &estado, 0);
+        printf("Soy el padre. Mi PID  es %d\n", getpid());        
+    }
+    else
+    {
+        fprintf(stderr, "Error ocurrido durante el fork()");
+    }
+}
