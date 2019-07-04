43e4b78eae58d481cdc5224e66065d89ce6cdd71
arroyopablo <pablo.arroyo@correounivalle.edu.co>
2019-07-03 23:30:27 -0500

    subiendo solucion del sexto punto

--- Ejercicios-Programacion-C05/pregunta6.c
+++ Ejercicios-Programacion-C05/pregunta6.c
@@ -9,14 +9,14 @@ int main()
     
     if (rc == 0) // hijo
     {
-        printf("Soy el proceso hijo. Mi PID es %d\n", getpid());
+        printf("Soy el proceso hijo y Mi PID es %d\n", getpid());
         exit(0);
     }
     else if (rc > 0) // padre
     {
         int estado;
         waitpid(rc, &estado, 0);
-        printf("Soy el padre. Mi PID  es %d\n", getpid());        
+        printf("Soy el padre y Mi PID  es %d\n", getpid());        
     }
     else
     {
