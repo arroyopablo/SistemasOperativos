df5c08a20e421108f09df81c31e796b5e7130175
arroyopablo <42813514+arroyopablo@users.noreply.github.com>
2019-07-09 16:20:01 -0500

    actualizanco waitpid

--- Ejercicios-Programacion-C05/pregunta6.c
+++ Ejercicios-Programacion-C05/pregunta6.c
@@ -8,14 +8,14 @@ int main()
     int rc = fork();
     
     if (rc == 0) // hijo
-    {
+    {   
+        int estado;
+        waitpid(rc, &estado, 0);
         printf("Soy el proceso hijo y Mi PID es %d\n", getpid());
         exit(0);
     }
     else if (rc > 0) // padre
-    {
-        int estado;
-        waitpid(rc, &estado, 0);
+    {        
         printf("Soy el padre y Mi PID  es %d\n", getpid());        
     }
     else
